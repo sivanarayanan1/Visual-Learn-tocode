87b1a71929ff084c78be8d7ea94cc88ab1c96552
Siva Narayanan <sivanarayanan1@gmail.com>
2025-06-07 02:59:51 +1000

    Making changes to index and main files, and commiting for the first time from VS Code

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ import { initProgram } from "./program.js";
 window.onload = () => {
   initProgram();
 };
+
+import { drawBoard } from "./board.js";
+
+const car = {
+  position: { x: 0, y: 0 },
+  direction: 0 // 0=up, 1=right, 2=down, 3=left
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  drawBoard(car);
+});
